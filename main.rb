58e2625e113ce1e0c706d70ef77b1aa37e55d26f
Jake Dam <robert.dam2@gmail.com>
2020-12-29 20:00:35 -0500

    updated player class to include symbol

--- main.rb
+++ main.rb
@@ -7,4 +7,11 @@ require_relative 'board.rb'
 test_board = Board.new
 
 test_board.display_board
+jake = Player.new("Jake")
+mike = Player.new("Mike")
+
+puts jake.player_number
+puts mike.player_number
+puts jake.symbol
+puts mike.symbol
 
--- player.rb
+++ player.rb
@@ -1,10 +1,17 @@
 class Player 
+  attr_reader :name, :player_number, :symbol 
 	@@number_of_players = 0
 
 	def initialize(name)
 		@name = name
 		@@number_of_players += 1
 		@player_number = @@number_of_players
+		if @player_number == 1
+      symbol = "X"
+    else
+      symbol = "O"
+    end
+    @symbol = symbol
 	end
 
 end

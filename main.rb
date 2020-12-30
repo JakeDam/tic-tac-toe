78dea9d8724fe8f703c998a24d82d85c4a514cf0
Jake Dam <robert.dam2@gmail.com>
2020-12-30 18:13:45 -0500

    refactored. Worked on winner? function

--- main.rb
+++ main.rb
@@ -4,12 +4,10 @@ require_relative 'game.rb'
 require_relative 'board.rb'
 
 
-test_board = Board.new
-test_board.display_board
-test_board.update_board(2, "X")
-test_board.display_board
-test_board.update_board(9, "O")
-test_board.display_board
+
+
+
+
 
 
 

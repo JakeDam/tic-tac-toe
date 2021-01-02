b65f657e13aefaa5091a01c438bb57bf7b60e854
Jake Dam <robert.dam2@gmail.com>
2021-01-01 21:13:36 -0500

    worked on play_round function

--- game.rb
+++ game.rb
@@ -1,9 +1,19 @@
 class Game 
+  include Interface
+
   def initialize(player1, player2, board)
     @player1 = player1
     @player2 = player2
     @board = board
   end
+
+  def play_round(player)
+    prompt(player)
+    while input = gets.chomp.to_i
+      if input.between?(1,9) == False 
+        puts "Please enter a valid number."
+
+        #TO DO
 end
 
 
--- interface.rb
+++ interface.rb
@@ -2,4 +2,8 @@ module Interface
   def greeting 
     puts "Let's play Tic-Tac-Toe!"
   end
+
+  def prompt(player)
+    puts "#{player.name} choose a space to place an #{player.symbol}."
+  end
 end
